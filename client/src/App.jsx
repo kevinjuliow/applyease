91b76b2680b5b7059b67941ca49c1eb1e149d1e8
Kevin Julio <kevjuliow@gmail.com>
2024-09-25 16:42:17 +0700

    fix login issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import './App.css'
-import Navbar from './assets/components/navbar/Navbar'
-import Login from './assets/pages/authentication/login/Login'
-import RegisterRole from './assets/pages/authentication/register/RegisterRole'
-import Landing from './assets/pages/landing/Landing'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import NotFoundPage from './assets/pages/notfound/NotFoundPage'
-import CompanyRegister from './assets/pages/authentication/register/CompanyRegister'
-import ApplicantRegister from './assets/pages/authentication/register/ApplicantRegister'
-import AuthenticatedMiddleware from './assets/lib/AuthenticatedMiddleware'
-import Dashboard from './assets/pages/dashboard/Dashboard'
-import DashboardContent from './assets/pages/dashboard/content/DashboardContent'
-
+import "./App.css";
+import Navbar from "./assets/components/navbar/Navbar";
+import Login from "./assets/pages/authentication/login/Login";
+import RegisterRole from "./assets/pages/authentication/register/RegisterRole";
+import Landing from "./assets/pages/landing/Landing";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import NotFoundPage from "./assets/pages/notfound/NotFoundPage";
+import CompanyRegister from "./assets/pages/authentication/register/CompanyRegister";
+import ApplicantRegister from "./assets/pages/authentication/register/ApplicantRegister";
+import AuthenticatedMiddleware from "./assets/lib/AuthenticatedMiddleware";
+import Dashboard from "./assets/pages/dashboard/Dashboard";
+import DashboardContent from "./assets/pages/dashboard/content/DashboardContent";
+import MainProvider from "./context/MainProvider";
 function App() {
   const protectedRoutes = [
     { path: "/register-role", element: <RegisterRole /> },
@@ -22,38 +27,36 @@ function App() {
   return (
     <>
       <Router>
-        <AuthenticatedMiddleware>
-          <Navbar />
-          <Routes>
-            <Route path={"/"} element={ <Landing /> } />
+        <MainProvider>
+          <AuthenticatedMiddleware>
+            <Navbar />
+            <Routes>
+              <Route path={"/"} element={<Landing />} />
 
-            <Route path={"/dashboard/*"} element={<Dashboard/>} >
-              <Route path={""} element={<DashboardContent/>}/>
-            
-            </Route>
+              <Route path={"/dashboard/*"} element={<Dashboard />}>
+                <Route path={""} element={<DashboardContent />} />
+              </Route>
 
+              {/* Dynamic routes for each route */}
+              {protectedRoutes.map(({ path, element }) => (
+                <Route
+                  key={path}
+                  path={path}
+                  element={
+                    <AuthenticatedMiddleware>{element}</AuthenticatedMiddleware>
+                  }
+                />
+              ))}
 
-            {/* Dynamic routes for each route */}
-            {protectedRoutes.map(({ path, element }) => (
-              <Route 
-                key={path} 
-                path={path} 
-                element={
-                  <AuthenticatedMiddleware>
-                    {element}
-                  </AuthenticatedMiddleware>
-                } 
-              />
-            ))}
-
-            {/* Not Found Page */}
-            <Route path="/notfound" element={<NotFoundPage />} />
-            <Route path="*" element={<Navigate to="/notfound" replace />} /> 
-          </Routes>
-        </AuthenticatedMiddleware>
+              {/* Not Found Page */}
+              <Route path="/notfound" element={<NotFoundPage />} />
+              <Route path="*" element={<Navigate to="/notfound" replace />} />
+            </Routes>
+          </AuthenticatedMiddleware>
+        </MainProvider>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
